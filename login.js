d29fd48e1832b1d727331a607c14467dd9fa8c50
Arpit14387 <93134428+Arpit14387@users.noreply.github.com>
2021-10-25 19:04:28 +0530

    1st one

---
+++ login.js
@@ -0,0 +1,29 @@
+function execute() {
+
+    var i=document.getElementById("ID").value;
+    var p=document.getElementById("pass").value;
+    
+    if(i==="arpit" & p==="14387")
+    {
+        var x=0;
+        window.location = "https://youtu.be/orJSJGHjBLI";
+    }
+    else
+    {
+    var x=1;
+    alert("try again...");
+    window.location = "login.html";
+    }
+    }
+    
+    
+    var input = document.getElementById("pass");
+    input.addEventListener("keyup", function(event) {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.key === 'Enter') {
+          // Cancel the default action, if needed
+          event.preventDefault();
+          // Trigger the button element with a click
+          execute();
+        }
+      });
